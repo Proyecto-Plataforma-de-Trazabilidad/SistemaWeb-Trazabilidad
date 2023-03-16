6b6428c3a5718c85093e239452dee6036f0d771c
SergioPiña <117691802+InfectedCupcake@users.noreply.github.com>
2023-03-15 23:14:41 -0600

    Se empezó con el movimiento entrega

---
+++ EntregasArchivos/optenerCampos.php
@@ -0,0 +1,18 @@
+<?php
+include("../conexion.php");
+//verificar conexion
+if ($enlace->connect_error) {
+    //mandar mensaje y salir
+    die("Conexion fallida: " . $enlace->connect_error);
+} else { //si todo funciona correctamente
+
+    session_start();
+
+
+
+}
+
+    
+
+
+?>
