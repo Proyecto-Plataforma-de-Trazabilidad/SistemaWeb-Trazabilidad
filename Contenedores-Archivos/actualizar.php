16c6d2d7b318c0914924cfaa7037219ae6cf1c14
SkalyBone <alejustogar3@gmail.com>
2023-03-27 18:16:58 -0600

    Menunuevo
    Jijijiji

--- Contenedores-Archivos/actualizar.php
+++ Contenedores-Archivos/actualizar.php
@@ -1,6 +1,6 @@
 <?php
 
-    include 'conexion.php';
+    include '../conexion.php';
 
     $id=$_POST['inid'];
     $tipoorigen=$_POST['intipoorigen'];
--- Layout/navMenu2.php
+++ Layout/navMenu2.php
@@ -25,14 +25,23 @@ $filas = mysqli_fetch_array($res);
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
+    <!--SWEET ALERT-->
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+
     <link rel="stylesheet" href="bootstrap-5.1.3-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="Datatables-1.11.3/css/dataTables.bootstrap5.min.css">
 
 
-    <link rel="stylesheet" href="../css/menucss.css">
-
+    <link rel="stylesheet" href="../Layout/menucss.css">
 
     <script src="https://kit.fontawesome.com/c65c1f4f0a.js" crossorigin="anonymous"></script>
+
+    <style>
+        a{
+            text-decoration: none;
+        }
+    </style>
 
 </head>
 
