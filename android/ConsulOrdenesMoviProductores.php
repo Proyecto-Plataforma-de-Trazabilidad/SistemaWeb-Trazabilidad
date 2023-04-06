1a3c7c5b93e6dba23b1365d2f955bb3fc9c82688
nayleaA <naygdz306@gmail.com>
2023-04-06 15:35:56 -0600

    Update ConsulOrdenesMoviProductores.php

--- android/ConsulOrdenesMoviProductores.php
+++ android/ConsulOrdenesMoviProductores.php
@@ -17,7 +17,7 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
         break;
         case 'DetOrdProductor':
             $id=$_POST['IdOrden'];
-            $query="SELECT *FROM detalleorden as DO WHERE IdOrden='$id'";
+            $query="SELECT DO.IdOrden,DO.Consecutivo,T.Concepto,DO.TipoEnvase,DO.Color,Do.CantidadPiezas FROM detalleorden as DO INNER JOIN tipoquimico as T on DO.IdTipoQuimico=T.IdTipoQuimico where DO.IdOrden='$id'";
             $resultado=$conn->prepare($query);
             $resultado->execute();
             $res = $resultado->fetchAll(PDO::FETCH_ASSOC);
