446782d4f5af0ff6231e9e248019f418876f898b
Shinon08 <126205540+Shinon08@users.noreply.github.com>
2023-04-06 16:56:28 -0600

    No se por que no sirven ya las condiciones
    no se

--- DistArchivos/funcionesDist.js
+++ DistArchivos/funcionesDist.js
@@ -32,7 +32,7 @@ $(document).ready(function(){
       });
 
 
-    $('#frm').submit(function(e){
+      $('#frm').submit(function(e){
         e.preventDefault();
         let formData = new FormData(this); //Este método trae todos los datos del form sin necesidad de leer el valor de cada campo
         
