78aa906e5254f67be14baf401b494d388e6c169a
Shinon08 <126205540+Shinon08@users.noreply.github.com>
2023-04-06 18:08:04 -0600

    CAT 3.0
    GG izi bots

--- ResponsablesArchivos/Insertar.php
+++ ResponsablesArchivos/Insertar.php
@@ -1,16 +1,15 @@
 <?php
-$nom=$_POST['nom'];
-$dom=$_POST['dom'];
-$cp=$_POST['cp'];
+$nom=$_POST['innom'];
+$dom=$_POST['indom'];
+$cp=$_POST['incp'];
 $muni=$_POST['jmr_contacto_municipio'];
 $est=$_POST['jmr_contacto_estado'];
-$tel=$_POST['tel'];
-$corr=$_POST['corr'];
-$edo=$_POST['edo'];
+$tel=$_POST['intel'];
+$corr=$_POST['incorr'];
+$edo=$_POST['inestado'];
 if($nom != null && $corr != null){
 $r="INSERT INTO responsablecat VALUES(NULL,'".$nom."','".$dom."','".$cp."','".$muni."','".$est."','".$tel."','".$corr."','".$edo."')";
 mysqli_query($enlace,$r);
-cargarTabla();
 }
 else{
     $data= null;
