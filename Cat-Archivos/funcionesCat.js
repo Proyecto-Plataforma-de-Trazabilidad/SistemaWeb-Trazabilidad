003f46f8573bf7fb06eea1d6a244c46ad518bd70
Shinon08 <126205540+Shinon08@users.noreply.github.com>
2023-04-11 19:12:23 -0600

    cas
    ac

--- Cat-Archivos/funcionesCat.js
+++ Cat-Archivos/funcionesCat.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
 
+    $.ajax({
+        url:'Cat-Archivos/metodosCat.php',
+        type:'POST',
+        data: {"tipo":""},
+        success:function(response){
+            $('#bodyTabla').html(response);
+            $('#tabla').DataTable({
+                scrollX:true,
+            });
+        }
+    });
+
     function combocat(){
         let tipoFuncion="combo1";
         let parametros={"tipo":tipoFuncion}
@@ -58,23 +70,4 @@ $(document).ready(function(){
         $('#frm').trigger('reset');
         
     });
-
-    $.ajax({
-        url:'Cat-Archivos/metodosCat.php',
-        data:{"tipo":""},
-        type:'POST',
-        success:function(response){
-            $('#bodyTabla').html(response);
-            $('#tabla').DataTable({
-                scrollX:true,
-            });
-        }
-
-    });
-    window.addEventListener('resize', function(event){
-        $('#tabla').DataTable().fnDestroy();
-        $('#tabla').DataTable({
-            scrollX:true,
-        });
-    },true);
 });
