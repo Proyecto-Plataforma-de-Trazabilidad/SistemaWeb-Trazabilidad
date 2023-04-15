2c68886501ec05cb66e9e1339daf8849679ad416
SkalyBone <alejustogar3@gmail.com>
2023-04-15 09:30:26 -0600

    se cancela
    u.u

--- loggin/recovery.php
+++ loggin/recovery.php
@@ -43,30 +43,8 @@ if ($row > 0) {
         //Content
         $mail->isHTML(true);                                  //Set email format to HTML
         $mail->Subject = 'Recuperar Password';
-        $mail->Body    = '<center>
-                            <div class="wave" style="height: 150px; overflow: hidden;" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M-0.84,151.48 C149.99,150.00 271.49,-49.98 501.97,82.41 L500.00,0.00 L0.00,0.00 Z" style="stroke: none; fill: #285430;"></path></svg></div>
-                            <h1>Campo Limpio</h1>
-                            <br>
-                            <p style="font-size:20px;
-                                    justify-content: center;
-                                    padding: 20px;">Este es un correo generado automáticamente para reestablecer la constraseña de su cuenta.</p>
-                            <br>
-                            <a href="http://campolimpiojal.com/loggin/changepsw.php?id=' . $row['IdUsuario'] . '">
-                            <button style="background: #285430;
-                                    color: #fff;
-                                    max-width: 200px;
-                                    cursor: pointer;
-                                    margin-bottom: 20px;
-                                    font-weight: 600px;
-                                    font-weight: bold;
-                                    padding: 20px;
-                                    border-radius: 20%;
-                                    font-size: 20px">Recuperar</button>
-                            </a>
-                        
-                            <div class="wave2" style="height: 150px; overflow: hidden;" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: #285430;"></path></svg></div>
-                        </center>';
-        $mail->AltBody = 'Abrir el correo desde una PC';
+        $mail->Body    = '<center>Este es un correo generado automáticamente para reestablecer la constraseña de su cuenta.<br><b>Por favor, visite la página <a href="http://campolimpiojal.com/loggin/changepsw.php?id=' . $row['IdUsuario'] . '"> Sistema de trazabilidad </a></b></center>';
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
         $mail->send();
 
