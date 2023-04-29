1a29b99c9430379091e5a4d275a29833b1e9fc22
nayleaA <naygdz306@gmail.com>
2023-04-28 20:33:14 -0600

    cambio 3, consultas extraviados android

--- android/ConsulExtraviadosMovimiProductores.php
+++ android/ConsulExtraviadosMovimiProductores.php
@@ -7,29 +7,29 @@ if($_SERVER['REQUEST_METHOD']=='POST')
     
     switch($_POST['opcion']){
         case 'EProductor':
-            $nombre=$_POST['nombre']; 
+            $correo=$_POST['correo'];
 
-            $query="SELECT E.TipoEnvaseVacio,E.CantidadPiezas,E.Aclaracion,E.fecha FROM extraviados as E INNER JOIN productores  as P on E.IdProductor=P.IdProductor where P.Nombre='$nombre'";
+            $query="SELECT E.TipoEnvaseVacio,E.CantidadPiezas,E.Aclaracion,E.fecha FROM extraviados as E INNER JOIN productores  as P on E.IdProductor=P.IdProductor where P.Correo='$correo'";
             $resultado=$conn->prepare($query);
             $resultado->execute();
             $res = $resultado->fetchAll(PDO::FETCH_ASSOC);
             break;
         case 'Pproductor':
-            $nombre=$_POST['nombre']; 
+            $correo=$_POST['correo'];
             $fi=$_POST['fi'];//fecha inicial
             $ff=$_POST['ff'];//fecha final
 
 
-            $query="SELECT E.TipoEnvaseVacio,E.CantidadPiezas,E.Aclaracion,E.fecha FROM extraviados as E INNER JOIN productores  as P on E.IdProductor=P.IdProductor where P.Nombre='$nombre' and  E.fecha BETWEEN '$fi' and '$ff'";
+            $query="SELECT E.TipoEnvaseVacio,E.CantidadPiezas,E.Aclaracion,E.fecha FROM extraviados as E INNER JOIN productores  as P on E.IdProductor=P.IdProductor where P.Correo='$correo' and  E.fecha BETWEEN '$fi' and '$ff'";
             $resultado=$conn->prepare($query);
             $resultado->execute();
             $res = $resultado->fetchAll(PDO::FETCH_ASSOC);
             break;
         case'TEProductor':
-            $nombre=$_POST['nombre']; 
+            $correo=$_POST['correo'];
             $e=$_POST['envase'];
 
-            $query="SELECT E.TipoEnvaseVacio,E.CantidadPiezas,E.Aclaracion,E.fecha FROM extraviados as E INNER JOIN productores  as P on E.IdProductor=P.IdProductor where P.Nombre='$nombre' and E.TipoEnvaseVacio='$e'";
+            $query="SELECT E.TipoEnvaseVacio,E.CantidadPiezas,E.Aclaracion,E.fecha FROM extraviados as E INNER JOIN productores  as P on E.IdProductor=P.IdProductor where P.Correo='$correo' and E.TipoEnvaseVacio='$e'";
             $resultado=$conn->prepare($query);
             $resultado->execute();
             $res = $resultado->fetchAll(PDO::FETCH_ASSOC);
