0ee873076389b163de018406513fbe9a7b0d6b8c
Ever <86848042+Kirersays1@users.noreply.github.com>
2023-04-30 10:52:21 -0600

    Update ConsulOrdenesMoviDistribuidores.php
    Update ConsulOrdenesMoviDistribuidores.php

--- android/ConsulOrdenesMoviDistribuidores.php
+++ android/ConsulOrdenesMoviDistribuidores.php
@@ -6,9 +6,9 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
     
     switch($_POST['opcion']){
         case 'OrdenDistribuidor':
-            $nombre=$_POST['nombre'];
+            $correo=$_POST['correo'];
             
-            $query="SELECT O.IdOrden,P.Nombre AS Productor,D.Nombre AS Distribuidor, O.NumFactura,O.NumReceta FROM ordenproductos AS O INNER JOIN productores AS P ON O.IdProductor=P.IdProductor INNER JOIN distribuidores AS D ON O.IdDistribuidor=D.IdDistribuidor WHERE P.Nombre='$nombre'";
+            $query="SELECT O.IdOrden,P.Nombre AS Productor,D.Nombre AS Distribuidor, O.NumFactura,O.NumReceta FROM ordenproductos AS O INNER JOIN productores AS P ON O.IdProductor=P.IdProductor INNER JOIN distribuidores AS D ON O.IdDistribuidor=D.IdDistribuidor WHERE D.Correo='$correo'";
             $resultado=$conn->prepare($query);
             $resultado->execute();
             $res = $resultado->fetchAll(PDO::FETCH_ASSOC);
@@ -26,7 +26,7 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
         case 'consulTQorden':
             $tq=$_POST['tq'];
             $nombre=$_POST['nombre'];
-            $query="SELECT O.IdOrden,D.Nombre AS Distribuidor, O.NumFactura,O.NumReceta FROM ordenproductos AS O INNER JOIN distribuidores AS D ON O.IdDistribuidor=D.IdDistribuidor INNER JOIN detalleorden AS DOR ON O.IdOrden=DOR.IdOrden INNER JOIN tipoquimico AS TQ ON DOR.IdTipoQuimico=TQ.IdTipoQuimico  INNER JOIN productores AS P ON O.IdProductor=P.IdProductor WHERE DOR.IdTipoQuimico=TQ.IdTipoQuimico AND TQ.Concepto='$tq' and P.Nombre='$nombre'";
+            $query="SELECT O.IdOrden,D.Nombre AS Distribuidor, P.Nombre as Productor, O.NumFactura,O.NumReceta FROM ordenproductos AS O INNER JOIN distribuidores AS D ON O.IdDistribuidor=D.IdDistribuidor INNER JOIN detalleorden AS DOR ON O.IdOrden=DOR.IdOrden INNER JOIN tipoquimico AS TQ ON DOR.IdTipoQuimico=TQ.IdTipoQuimico  INNER JOIN productores AS P ON O.IdProductor=P.IdProductor WHERE DOR.IdTipoQuimico=TQ.IdTipoQuimico AND TQ.Concepto='$tq' and D.Correo='$correo'";
             $resultado=$conn->prepare($query);
             $resultado->execute();
             $res = $resultado->fetchAll(PDO::FETCH_ASSOC);
@@ -44,9 +44,9 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
             
         case 'consulEorden':
              $e=$_POST['envase'];
-             $nombre=$_POST['nombre'];
+             $correo=$_POST['correo'];
 
-            $query="SELECT O.IdOrden,D.Nombre AS Distribuidor, O.NumFactura,O.NumReceta FROM ordenproductos AS O INNER JOIN distribuidores AS D ON O.IdDistribuidor=D.IdDistribuidor INNER JOIN detalleorden AS DOR ON O.IdOrden=DOR.IdOrden  INNER JOIN productores AS P ON O.IdProductor=P.IdProductor WHERE DOR.TipoEnvase='$e' and P.Nombre='$nombre' GROUP BY O.IdOrden";
+            $query="SELECT O.IdOrden,D.Nombre AS Distribuidor,P.Nombre as Productor, O.NumFactura,O.NumReceta FROM ordenproductos AS O INNER JOIN distribuidores AS D ON O.IdDistribuidor=D.IdDistribuidor INNER JOIN detalleorden AS DOR ON O.IdOrden=DOR.IdOrden  INNER JOIN productores AS P ON O.IdProductor=P.IdProductor WHERE DOR.TipoEnvase='$e' and D.Correo='$correo' GROUP BY O.IdOrden";
             $resultado=$conn->prepare($query);
             $resultado->execute();
             $res = $resultado->fetchAll(PDO::FETCH_ASSOC);
@@ -62,11 +62,11 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
         break;
         
         case 'consulOfecha':
-            $nombre=$_POST['nombre'];
+            $correo=$_POST['correo'];
             $fi=$_POST['fi'];//fecha inicial
             $ff=$_POST['ff'];//fecha final
 
-            $query="SELECT O.IdOrden,P.Nombre AS Productor,D.Nombre AS Distribuidor, O.NumFactura,O.NumReceta FROM ordenproductos AS O INNER JOIN productores AS P ON O.IdProductor=P.IdProductor INNER JOIN distribuidores AS D ON O.IdDistribuidor=D.IdDistribuidor where O.Fecha BETWEEN '$fi' and '$ff' and P.Nombre='$nombre'";
+            $query="SELECT O.IdOrden,P.Nombre AS Productor,D.Nombre AS Distribuidor, O.NumFactura,O.NumReceta FROM ordenproductos AS O INNER JOIN productores AS P ON O.IdProductor=P.IdProductor INNER JOIN distribuidores AS D ON O.IdDistribuidor=D.IdDistribuidor where O.Fecha BETWEEN '$fi' and '$ff' and D.Correo='$correo'";
             $resultado=$conn->prepare($query);
             $resultado->execute();
             $res = $resultado->fetchAll(PDO::FETCH_ASSOC);
