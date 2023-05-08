29631b385b9db266a402bad9b2db0a49c97d3668
nayleaA <naygdz306@gmail.com>
2023-05-08 15:48:00 -0600

    un cambio mas

--- PruebaReportesNay/funcionReporte1.js
+++ PruebaReportesNay/funcionReporte1.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
         let prod=document.getElementById("inprod").value;
         let tipofuncion="registrar";
 
-        console.log(prod);
+        console.log(prod)
 
         let parametros={"prod":prod,"tipo":tipofuncion}
         $.ajax({
@@ -25,38 +25,10 @@ $(document).ready(function(){
             data:parametros,
             type:'POST',
             success:function(response){
-                const respuesta = response.json();
-                const $grafica = document.querySelector("#myChart");
-                const etiquetas = respuesta.etiquetas; 
-
-                const datos = {
-                    label: "Total piezas del Productor",
-                    // Pasar los datos igualmente desde PHP
-                    data: respuesta.datos, // <- Aquí estamos pasando el valor traído usando AJAX
-                    backgroundColor: 'rgba(54, 162, 235, 0.2)', // Color de fondo
-                    borderColor: 'rgba(54, 162, 235, 1)', // Color del borde
-                    borderWidth: 1, // Ancho del borde
-                };
-                new Chart($grafica, {
-                    type: 'bar', // Tipo de gráfica
-                    data: {
-                        labels: etiquetas,
-                        datasets: [
-                            datos,
-                        ]
-                    },
-                    options: {
-                        scales: {
-                            yAxes: [{
-                                ticks: {
-                                    beginAtZero: true
-                                }
-                            }],
-                        },
-                    }
-                });
-
-                //$('#myChart').html(response);
+               var valores=eval(response);
+               var e=valores[0];
+               console.log(e);
+            
             }
         });
         $('#frm').trigger('reset');
