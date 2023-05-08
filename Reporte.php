82468a19613e408f5645c9a9b2423b6fdbb6586b
nayleaA <naygdz306@gmail.com>
2023-05-08 13:45:23 -0600

    acomoando mi codigo

--- Reporte.php
+++ Reporte.php
@@ -1,7 +1,8 @@
 <?php
-include "Layout/navMenu.php";
+include "Layout/navMenu.php";//menu
+
 include 'conexion.php';
-$r = "SELECT * FROM usuarios where Correo = '$varses'";
+$r = "SELECT * FROM usuarios where Correo = '$varses'";//usuario del menu, de donde sale varses? ni idea.
 $res = mysqli_query($enlace, $r);
 $filas = mysqli_fetch_array($res);
 ?>
@@ -13,17 +14,17 @@ $filas = mysqli_fetch_array($res);
     
     <form class="row g-4 container-fluid" id="frm" method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" onsubmit="return valdez()">
 
-      <div class="col-sm-3">
+    <!--Combo, funciona con ajax, enlazado a js y el js a un php, esta en los scripts-->
+      <div class="col-sm-3">  
         <label for="inprod" class="form-label">Productor</label>
         <select name="inprod" id="inprod" class="form-select">
-
         </select>
       </div>
 
+      <!--El boton que consultara, tambien llama al jsfuncion  y el js al php metodo-->
       <div class="col-sm-3">
         <button type="submit" class="btn btn-success" onclick="" name="Registrar">Registrar</button>
       </div>
-      
 
     </form>
 
@@ -33,6 +34,7 @@ $filas = mysqli_fetch_array($res);
 
     <script>
        
+    //esto nose para que funciona 
     function obtenerTamaño() {
       let ancho = document.documentElement.clientWidth;
       ancho.addListener("change", function(event) {
@@ -64,6 +66,7 @@ $filas = mysqli_fetch_array($res);
     <script type="text/javascript" src="PruebaReportesNay/funcionReporte1.js"></script>
     <script src="Layout/menujs.js"></script>
 
+    <!--Liberia delas graficas-->
     <script src="https://cdn.jsdelivr.net/npm/chart.js@3.0.0/dist/chart.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2.0.0"></script>
 
