29aec3511cae0ef65f1311b856638177dc7ad29c
nayleaA <naygdz306@gmail.com>
2023-05-08 17:15:15 -0600

    creo que ya ahora si se ve el grafico

--- PruebaReportesNay/funcionReporte1.js
+++ PruebaReportesNay/funcionReporte1.js
@@ -28,9 +28,33 @@ $(document).ready(function(){
             data:parametros,
             type:'POST',
             success:function(response){
-               
-            
                console.log(response);
+
+        
+               var valor = [];
+
+                for (var i in data) { valor.push(data[i].TotalPiezas); }
+
+                    var chartdata = {
+                        labels: "Total Piezas",
+                        datasets: [
+                            {
+                                label: 'Piezas',
+                                backgroundColor: '#49e2ff',
+                                borderColor: '#46d5f1',
+                                hoverBackgroundColor: '#CCCCCC',
+                                hoverBorderColor: '#666666',
+                                data: valor
+                            }
+                        ]
+                    };
+
+                    var graphTarget = $("#myChart");
+
+                    var barGraph = new Chart(graphTarget, {
+                        type: 'bar',
+                        data: chartdata
+                    });
             
             }
         });
