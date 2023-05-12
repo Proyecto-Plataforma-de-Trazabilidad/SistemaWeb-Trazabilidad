20dfbc8f66515b1f38c10e0c8962961cf3625aa0
nayleaA <naygdz306@gmail.com>
2023-05-12 08:14:49 -0600

    Reportes Android
    Query

---
+++ android/Reportes/ConsuReportes.php
@@ -0,0 +1,19 @@
+<?php
+if($_SERVER['REQUEST_METHOD']=='POST')
+{ 
+    include('database.php');
+    $conn = new PDO('mysql:host='.$host_name.';dbname='.$database,$host_user,$host_password);
+
+    
+    switch($_POST['opcion']){
+        case 'RepGCat':
+            $query="select count(*) as Total_CATs from `centroacopiotemporal`";
+            $resultado=$conn->prepare($query);
+            $resultado->execute();
+            $res = $resultado->fetchAll(PDO::FETCH_ASSOC);
+            break;
+    }
+        echo json_encode($res);
+        $conn = null; //Limpia la conexión
+}
+?>
